c2faf623d7c35ef65788882a1fcba59fec953052
xylops <rankine1607@gmail.com>
2016-11-11 15:45:06 +0800

    add Todo component

--- webpack.config.js
+++ webpack.config.js
@@ -22,8 +22,12 @@ module.exports = {
     },
     resolve: {
         root: __dirname,
+        modulesDirectories: [
+            'node_modules',
+            './app/components'
+        ],
         alias:{
-            applicationStyles:'app/styles/app.scss',    
+            applicationStyles:'app/styles/app.scss',
         },
         extensions: ['', '.js', '.jsx']
     },
