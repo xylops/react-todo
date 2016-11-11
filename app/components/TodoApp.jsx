51ed3ff5a5dc88d4185d3fbcffa8a0148921d5ab
xylops <rankine1607@gmail.com>
2016-11-11 16:43:12 +0800

    Add stat todo list

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,32 @@
 var React = require('react');
+var TodoList = require('TodoList')
 
 var TodoApp = React.createClass({
+    getInitialState:function(){
+        return {
+            todos:[
+                {
+                    id:1,
+                    text:'walk the dog'
+                },{
+                    id:2,
+                    text:'Clean the yard'
+                },{
+                    id:3,
+                    text:'adopt a cat'
+                },{
+                    id:4,
+                    text:'download new games'
+                }
+            ]
+        };
+    },
     render:function(){
+        var {todos} = this.state
+
         return(
             <div>
-                TodoApp.jsx
+                <TodoList todos={todos}/>
             </div>
         )
     },
