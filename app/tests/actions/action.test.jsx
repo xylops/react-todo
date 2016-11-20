5a343b59761a6122573d288419dc4754bd1a86ba
Xylops <rankine1607@gmail.com>
2016-11-20 14:20:53 +0800

    adding actions generator and test

---
+++ app/tests/actions/action.test.jsx
@@ -0,0 +1,45 @@
+var expect = require('expect');
+var actions = require('actions');
+
+describe('Actions', ()=>{
+    it('should generate search text actions', ()=>{
+        var action = {
+            type:'SET_SEARCH_TEXT',
+            searchText: 'Some Search Text'
+        }
+
+        var res = actions.setSearchText(action.searchText);
+
+        expect(res).toEqual(action)
+    });
+
+    it('should generate add todo action',()=>{
+        var action = {
+            type:'ADD_TODO',
+            text: 'Things to do'
+        }
+        var res = actions.addTodo(action.text);
+
+        expect(res).toEqual(action)
+    })
+
+    it('should generate toggle show competed action',()=>{
+        var action = {
+            type:'TOGGLE_SHOW_COMPLETE',
+        }
+        var res = actions.toggleShowComplete();
+
+        expect(res).toEqual(action)
+    })
+
+    it('should generate toggle todo action',()=>{
+        var action = {
+            type:'TOGGLE_TODO',
+            id: '2'
+        }
+        var res = actions.toggleTodo(action.id);
+
+        expect(res).toEqual(action)
+    })
+
+})
